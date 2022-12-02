688d2bc054f165ba5f50b6f54e91ae9c437836cb
Hafiz Umar Barlas <umarbarlas4@gmail.com>
2022-12-02 21:52:33 +0500

    Folder structure and theme files setup

---
+++ src/pages/global/Topbar.jsx
@@ -0,0 +1,19 @@
+import {Box, IconButton, useTheme} from '@mui/material'
+import { useContext } from 'react';
+import { ColorModeContext, tokens } from '../../theme';
+import InputBase from '@mui/material';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Topbar = () => {
+    return (
+        <div>Topbar</div>
+    )
+}
+
+export default Topbar;
